6d190b2b1ca54f93420d864b091024d88d0bea44
cathy <cathy141153@gmail.com>
2023-02-26 15:51:55 +0800

    add files

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://cathy7485.github.io
+# git push -f git@github.com:cathy7485/cathy7485.github.io.git master
+
+# 部署到 https://cathy7485.github.io/vite-deploy-sample
+git push -f https://github.com/cathy7485/vite-deploy-sample.git master:gh-pages
+
+cd -
+
